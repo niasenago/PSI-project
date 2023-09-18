18eb5ed638da5e557dccace4348bdd5542f38acb
SarunasJJ <sarunas.jemeljanovas@gmail.com>
2023-09-18 18:44:22 +0300

    Added post model, controller and view

---
+++ CollabApp/CollabApp.mvc/Controllers/PostController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CollabApp.mvc.Controllers
+{
+    public class PostController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CollabApp/CollabApp.mvc/Models/Post.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CollabApp.mvc.Models
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Author { get; set; }
+        public DateTime DatePosted { get; set; } = DateTime.Now;
+
+    }
+}
