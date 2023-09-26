c273a893dc128b58bd088fe28f6ecd5103185632
Artiom <h.artiom4ik@gmail.com>
2023-09-26 22:51:20 +0300

    fix incorrect post id initialization.

--- CollabApp/CollabApp.mvc/Controllers/JsonDbController.cs
+++ CollabApp/CollabApp.mvc/Controllers/JsonDbController.cs
@@ -8,6 +8,7 @@ namespace CollabApp.mvc.Controllers
 {
     public class JsonDbController : IDBAccess
     {
+        public int itemId { get; set; }
         private string dbFilename { get; set; }
         private string dbPath { get; set; }
         private string fullDbPath { get; set; }
@@ -17,18 +18,24 @@ namespace CollabApp.mvc.Controllers
             this.dbFilename = dbFilename;
             this.dbPath = dbPath;
             this.fullDbPath = Path.Combine(dbPath, dbFilename);
+            InitializeItemId();
         }
         public JsonDbController(string fullDbPath)
         {
             this.fullDbPath = fullDbPath;
+            InitializeItemId();
         }
         //every time then this method is called it overwrite the entire JSON file with the new data 
         public void AddPost(Post post) {
+            post.Id = itemId;   //every time then post is created post id is set
             List<Post> posts = GetAllPosts();
             posts.Add(post);
 
+            this.itemId++;      //then post id is incremented
+
             string jsonString = JsonSerializer.Serialize(posts);
             File.WriteAllText(fullDbPath, jsonString);
+            
         }
 
         public Post GetPostById(int id) {
@@ -51,6 +58,15 @@ namespace CollabApp.mvc.Controllers
                 /* TO-DO If the file does not exist */
                 return null;
             }
+        }
+
+        /**TEMPORARILY*/
+        private void InitializeItemId()
+        {
+            List<Post> posts = GetAllPosts();
+            /*If there are posts, this part calculates the maximum Id value from the posts list and adds 1 to it.*/
+            this.itemId = posts.Count > 0 ? posts.Max(p => p.Id) + 1 : 1;
+            
         }
 
     }
