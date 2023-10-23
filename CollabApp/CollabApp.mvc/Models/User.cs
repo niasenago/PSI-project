185d049f27f9f6a6b8ee155457344fc562530070
Brigita Bruškytė <114769087+ClawOfNyx@users.noreply.github.com>
2023-10-23 21:44:52 +0300

    created user class

---
+++ CollabApp/CollabApp.mvc/Models/User.cs
@@ -0,0 +1,25 @@
+using CollabApp.mvc.Utilities;
+using System.ComponentModel.DataAnnotations;
+
+namespace CollabApp.mvc.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Username { get; set; }
+
+        public User(string username)
+        {
+            // Initialize the ID when creating a new Post object.
+            this.Id = GenerateUniqueId();
+            Username = username;
+
+        }
+        private int GenerateUniqueId()
+        {
+            return IdGenerator.GenerateUserId();
+        }
+    }
+}
