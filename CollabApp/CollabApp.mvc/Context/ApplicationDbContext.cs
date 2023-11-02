8fcf9b80c4c61088da1d6b70aa27f0c2704a740b
Artiom <h.artiom4ik@gmail.com>
2023-11-02 11:41:55 +0200

    update ApplicationDbContext and set up table relationships

--- CollabApp/CollabApp.mvc/Context/ApplicationDbContext.cs
+++ CollabApp/CollabApp.mvc/Context/ApplicationDbContext.cs
@@ -8,6 +8,20 @@ namespace CollabApp.mvc.Context
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
         }
-        public DbSet<Post>Posts {get;set;}
+        public virtual DbSet<Post>Posts {get;set;}
+        public virtual DbSet<Comment>Comments{get;set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // connect post with comments  1 - Many relationship
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Comment>(entity => {    //for a Comment entity
+                entity.HasOne(p => p.Post)              //for every comment it has one post
+                    .WithMany(c => c.Comments)          //post has many comments
+                    .HasForeignKey(x => x.PostId)       //specify under which id it needs to connect post and comments
+                    .OnDelete(DeleteBehavior.Restrict)  //not allowed to delete if there is a connection
+                    .HasConstraintName("FK_Comment_Post");
+            });
+        }
     }
 }
