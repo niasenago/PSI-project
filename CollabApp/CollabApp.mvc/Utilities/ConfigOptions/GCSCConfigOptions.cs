1af77850c47940a51cd85bd9e464fed4c5b735c5
Artiom <h.artiom4ik@gmail.com>
2023-11-06 22:40:20 +0200

    add Cloudservice class and implement methods

---
+++ CollabApp/CollabApp.mvc/Utilities/ConfigOptions/GCSCConfigOptions.cs
@@ -0,0 +1,10 @@
+namespace CollabApp.mvc.Utilities
+{
+    public class GCSConfigOptions
+    {
+        public string? GCPStorageAuthFile { get; set; }
+        public string? GoogleCloudStorageBucketName { get; set; }
+
+
+    }
+}
