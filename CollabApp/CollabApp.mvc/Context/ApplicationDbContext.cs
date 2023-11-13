25ed589485851e1d05b8e168da9d623fe1d3e56a
Artiom <h.artiom4ik@gmail.com>
2023-11-13 20:58:05 +0200

    modify applicationDbContext and HomeController to work with boards

--- CollabApp/CollabApp.mvc/Context/ApplicationDbContext.cs
+++ CollabApp/CollabApp.mvc/Context/ApplicationDbContext.cs
@@ -10,6 +10,8 @@ namespace CollabApp.mvc.Context
         }
         public virtual DbSet<Post>Posts {get;set;}
         public virtual DbSet<Comment>Comments{get;set;}
+        public virtual DbSet<Board>Boards{get;set;}
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
--- CollabApp/CollabApp.mvc/Controllers/HomeController.cs
+++ CollabApp/CollabApp.mvc/Controllers/HomeController.cs
@@ -1,21 +1,25 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using CollabApp.mvc.Models;
+using CollabApp.mvc.Context;
 
 namespace CollabApp.mvc.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly ApplicationDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     public IActionResult Index()
     {
-        return View();
+        var boards = _context.Boards.ToList();
+        return View(boards);
     }
 
     public IActionResult Chat()
@@ -27,5 +31,22 @@ public class HomeController : Controller
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+    [HttpPost]
+    public IActionResult CreateBoard(Board board)
+    {
+        if (ModelState.IsValid)
+        {
+            // Set the creation date before adding to the database
+            board.CreationDate = DateTime.UtcNow;
+
+            _context.Boards.Add(board);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index"); // Redirect to the appropriate action after successful creation
+        }
+
+        // If the model state is not valid, return to the create view with validation errors
+        return View("Create", board);
     }
 }
