2c3c8e3ab2e67afc9c27991d510e148a04338a40
Artiom <h.artiom4ik@gmail.com>
2023-11-17 17:26:01 +0200

    implement GetAllAsync,GetAsync,UpdateEntity methods in PostRepository class

--- CollabApp/CollabApp.mvc/Repo/PostRepository.cs
+++ CollabApp/CollabApp.mvc/Repo/PostRepository.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CollabApp.mvc.Context;
 using CollabApp.mvc.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CollabApp.mvc.Repo
 {
@@ -12,25 +13,57 @@ namespace CollabApp.mvc.Repo
         public PostRepository(ApplicationDbContext dbContext) : base(dbContext)
         {
         }
-
-        public Task<bool> AddEntity(Post entity)
-        {
-            throw new NotImplementedException();
-        }
-
         public override Task<List<Post>> GetAllAsync()
         {
             return base.GetAllAsync();
         }
 
-        public Task<List<Post>> GetAsync(int id)
+        public override async Task<Post> GetAsync(int id)
         {
-            throw new NotImplementedException();
+            return await DbSet.FirstOrDefaultAsync(item => item.Id == id);
         }
 
-        public Task<bool> UpdateEntity(Post entity)
+        public override async Task<bool> AddEntity(Post entity)
         {
-            throw new NotImplementedException();
+            try 
+            {
+                await DbSet.AddAsync(entity);
+                return true;
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+
+        public override async Task<bool> UpdateEntity(Post entity)
+        {
+            try
+            {
+                var existData = await DbSet.FirstOrDefaultAsync(item => item.Id == entity.Id);
+                if(existData != null)
+                {
+                    existData.Id = entity.Id;
+                    existData.Title = entity.Title;
+                    entity.Description = entity.Description;
+                    existData.Author = entity.Author;
+                    existData.BoardId = entity.BoardId;
+                    existData.SavedFileName = entity.SavedFileName;
+                    existData.Photo = entity.Photo;
+                    existData.SignedUrl = entity.SignedUrl;
+                    existData.SavedUrl = entity.SavedUrl;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
         }
     }
 }
