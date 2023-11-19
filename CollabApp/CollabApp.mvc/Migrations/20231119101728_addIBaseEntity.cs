4cc267dc6217daf681361ae711c3440db72c7a0b
Artiom <h.artiom4ik@gmail.com>
2023-11-19 12:33:07 +0200

    add migration

---
+++ CollabApp/CollabApp.mvc/Migrations/20231119101728_addIBaseEntity.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CollabApp.mvc.Migrations
+{
+    /// <inheritdoc />
+    public partial class addIBaseEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreationDate",
+                table: "Boards",
+                newName: "CreationDateTime");
+
+            migrationBuilder.RenameColumn(
+                name: "BoardId",
+                table: "Boards",
+                newName: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreationDateTime",
+                table: "Boards",
+                newName: "CreationDate");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Boards",
+                newName: "BoardId");
+        }
+    }
+}
