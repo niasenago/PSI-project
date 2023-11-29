638d4c962acd0a53203919db9af372d3c9d3e34c
Artiom <h.artiom4ik@gmail.com>
2023-11-29 16:09:48 +0200

    modify RegisterViewModel to have confirmed password property

--- CollabApp/CollabApp.mvc/Models/User.cs
+++ CollabApp/CollabApp.mvc/Models/User.cs
@@ -45,7 +45,13 @@ namespace CollabApp.mvc.Models
         public string Username { get; set; }
 
         [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
     }
 
 }
