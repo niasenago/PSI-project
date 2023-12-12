3645ad6c735e2006141e392abeda5deec4ca8ed7
SarunasJJ <sarunas.jemeljanovas@gmail.com>
2023-12-12 15:17:35 +0200

    integration tests

---
+++ CollabApp/CollabApp.UnitTests/IntegrationTests/Context/ApplicationDbContextTests.cs
@@ -0,0 +1,32 @@
+﻿
+using CollabApp.mvc.Context;
+using CollabApp.mvc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CollabApp.Tests.IntegrationTests.Context
+{
+    public class ApplicationDbContextTests
+    {
+        [Fact]
+        public void CanSaveAndGetPost()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+
+            using (var dbContext = new ApplicationDbContext(options))
+            {
+                var post = new Post { Id = 1, BoardId = 1, Title = "Post 1", AuthorId = 1, Description = "Description 1" };
+                dbContext.Posts.Add(post);
+                dbContext.SaveChanges();
+            }
+            var postId = 1;
+
+            using (var dbContext = new ApplicationDbContext(options))
+            {
+                var retrievedPost = dbContext.Posts.FirstOrDefault(post => post.Id == postId);
+                Assert.NotNull(retrievedPost);
+            }
+        }
+    }
+}
---
+++ CollabApp/CollabApp.UnitTests/IntegrationTests/Repo/UserRepositoryTests.cs
@@ -0,0 +1,60 @@
+﻿using CollabApp.mvc.Context;
+using CollabApp.mvc.Models;
+using CollabApp.mvc.Repo;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollabApp.Tests.IntegrationTests.Repo
+{
+    public class UserRepositoryTests
+    {
+        [Fact]
+        public async Task GetUserByUsernameAsync_ReturnsUser()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+
+            using (var dbContext = new ApplicationDbContext(options))
+            {
+                dbContext.Users.Add(new User { Id = 1, Username = "TestUser", PasswordHash = "1234", Salt = "abc" });
+                dbContext.Users.Add(new User { Id = 2, Username = "AnotherUser", PasswordHash = "12345", Salt = "abcd" });
+                await dbContext.SaveChangesAsync();
+            }
+
+            using (var dbContext = new ApplicationDbContext(options))
+            {
+                var userRepository = new UserRepository(dbContext);
+                var result = await userRepository.GetUserByUsernameAsync("TestUser");
+
+                Assert.NotNull(result);
+                Assert.Equal("TestUser", result.Username);
+            }
+        }
+        [Fact]
+        public async Task IsUsernameTakenAsync_ReturnsBool()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+
+            using (var dbContext = new ApplicationDbContext(options))
+            {
+                dbContext.Users.Add(new User { Id = 1, Username = "TestUser", PasswordHash = "1234", Salt = "abc" });
+                await dbContext.SaveChangesAsync();
+            }
+
+            using (var dbContext = new ApplicationDbContext(options))
+            {
+                var userRepository = new UserRepository(dbContext);
+                var result = await userRepository.IsUsernameTakenAsync("TestUser");
+
+                Assert.True(result);
+            }
+        }
+    }
+}
